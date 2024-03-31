267940f13444590967391da922e8d4affbd647d0
UnikMask <alexandre.guerville@hotmail.com>
2024-03-31 18:41:22 +0200

    fix(raycaster): Fix voxel map texture image being 2D

--- plover/src/Texture.cpp
+++ plover/src/Texture.cpp
@@ -401,18 +401,27 @@ void createTexture(VulkanContext &context, VoxelMap voxelmap, Texture &texture) 
     texture.imageSize = voxelmap.width * voxelmap.height * voxelmap.depth * voxelmap.stride();
     texture.format = voxelmap.vulkanFormat();
 
-    CreateImageInfo imageInfo {
-        .width = voxelmap.width,
-        .height = voxelmap.height,
-        .depth = voxelmap.depth,
-        .layers = 1,
+    VkImageCreateInfo imageInfo {
+        .sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO,
+        .imageType = VK_IMAGE_TYPE_3D,
         .format = texture.format,
+        .extent = {
+            .width = voxelmap.width,
+            .height = voxelmap.height,
+            .depth = voxelmap.depth},
+        .mipLevels = 1,
+        .arrayLayers = 1,
+        .samples = VK_SAMPLE_COUNT_1_BIT,
         .tiling = VK_IMAGE_TILING_OPTIMAL,
         .usage = VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
-        .properties = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
-        .vmaFlags = (VmaAllocationCreateFlagBits) 0};
+        .sharingMode = VK_SHARING_MODE_EXCLUSIVE,
+        .initialLayout = VK_IMAGE_LAYOUT_UNDEFINED };
+    VmaAllocationCreateInfo allocCreateInfo {
+        .flags = 0,
+        .usage = VMA_MEMORY_USAGE_AUTO,
+        .requiredFlags = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT };
 
-    context.createImage(imageInfo, texture.image, texture.allocation);
+    vmaCreateImage(context.allocator, &imageInfo, &allocCreateInfo, &texture.image, &texture.allocation, nullptr);
     texture.copyVoxelmap(context, voxelmap);
 
     CreateImageViewInfo imageViewInfo {
